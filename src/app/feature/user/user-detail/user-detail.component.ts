61b7b0131c7b98388bd52bbbafe2ce5fed085695
Chris Mason <m1achris43@gmail.com>
2021-01-20 20:08:12 -0500

    Edited user info

--- src/app/feature/user/user-detail/user-detail.component.ts
+++ src/app/feature/user/user-detail/user-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/model/user.class';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-user-detail',
@@ -6,10 +9,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-detail.component.css']
 })
 export class UserDetailComponent implements OnInit {
+  title = "User Detail";
+  user: User = null;
+  userId: number = 0;
 
-  constructor() { }
+  constructor(private userSvc: UserService, private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit() {
-  }
+  ngOnInit(): void {
+   // get the id from the url
+   this.route.params.subscribe(
+    parms => {this.userId = parms['id'];
+    console.log("UserID = " +  this.user);
+  });
+  // get user by id
+  this.userSvc.getByID(this.userId).subscribe(
+    resp => {
+      this.user = resp as User;
+      console.log("User", this.user);
+    },
+    err => {
+      console.log(err);
+    }
+  );
+}
+
+delete() {
+  // delete the user from the DB
+  this.userSvc.delete(this.user.id).subscribe(
+    resp => {
+      this.user = resp as User;
+      console.log("User delete", this.user);
+      // forward to the user list component
+      this.router.navigateByUrl("/user-list");
+    },
+    err => {
+      console.log(err);
+    }
+  );
+}
 
 }
--- src/app/feature/user/user-edit/user-edit.component.ts
+++ src/app/feature/user/user-edit/user-edit.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/model/user.class';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-user-edit',
@@ -6,10 +9,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
+  title = "User Edit";
+  user: User = null;
+  userId: number = 0;
+  submitBtnTitle = "Save";
 
-  constructor() { }
+  constructor(private userSvc: UserService, private router: Router, private route: ActivatedRoute) { }
 
-  ngOnInit() {
-  }
+  ngOnInit(): void {
+  // get the id from the url
+  this.route.params.subscribe(
+    parms => {this.userId = parms['id'];
+  console.log("UserID = " + this.userId);
+  });
+  // get user by id
+  this.userSvc.getByID(this.userId).subscribe(
+    resp => {
+      this.user = resp as User;
+      console.log("User", this.user);
+    },
+    err => {
+      console.log(err);
+    }
+  );
+}
+
+save() {
+  // save the user to the DB
+  this.userSvc.update(this.user).subscribe(
+    resp => {
+      this.user = resp as User;
+      console.log("User updated", this.user);
+      // forward to the user list componenet
+      this.router.navigateByUrl("/user-list");
+    },
+    err => {
+      console.log(err);
+    }
+  );
+}
 
 }
